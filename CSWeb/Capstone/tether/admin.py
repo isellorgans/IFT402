f30b576f69fdf3e2eadb173c54d382c447d3b1ea
isellorgans <sam.hughel@gmail.com>
2017-02-23 00:56:21 -0700

    Add files via upload

---
+++ CSWeb/Capstone/tether/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from tether.models import UserProfile, League, RecentMatches
+
+
+class LeagueAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('league_name',)}
+
+admin.site.register(UserProfile)
+admin.site.register(League, LeagueAdmin)
+admin.site.register(RecentMatches)
