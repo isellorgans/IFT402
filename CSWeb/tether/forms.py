2c5c6f3ed4ccaa91efeaf594af15ecd7388a8396
Eagle1191 <Werise92@gmail.com>
2017-03-29 00:41:05 -0700

    Add files via upload

--- CSWeb/tether/forms.py
+++ CSWeb/tether/forms.py
@@ -1,4 +1,4 @@
-from tether.models import UserProfile
+from tether.models import UserProfile1, League
 from django.contrib.auth.models import User
 from django import forms
 
@@ -6,7 +6,9 @@ from django import forms
 # The form for user profiles
 class UserForm(forms.ModelForm):
     # Keeps the password hidden while typing
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control input-md'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control input-md'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control input-md'}))
 
     class Meta:
         model = User
@@ -15,6 +17,16 @@ class UserForm(forms.ModelForm):
 
 # Additional form for more attributes to user profile.
 class UserProfileForm(forms.ModelForm):
+    region = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control input-md'}))
+
     class Meta:
-        model = UserProfile
+        model = UserProfile1
         fields = ('region',)
+
+
+class LeagueForm(forms.ModelForm):
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = League
+        fields = ('league_name', 'region', 'skill_level', 'password', 'slug')
